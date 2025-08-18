f52ad40911d353f5e366c142688b092df56c882a
Lina Brown <brownli2@appstate.edu>
2025-08-18 12:01:37 -0700

    fix ingress block

--- main.tf
+++ main.tf
@@ -30,10 +30,7 @@ resource "aws_instance" "web" {
   }
 }
 
-module "security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-}
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -41,7 +38,7 @@ module "blog_sg" {
   name = "blog_new"
   vpc_id      = data.aws_vpc.default.id
 
-  ingress_rules = ["https-80-tcp", "https-443-tcp"]
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules = ["all-all"]
